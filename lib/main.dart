991556385ee1c4eb9f001a69c6d81ee00a308d32
JakeOJeff <realjakeojeff@gmail.com>
2025-06-14 00:57:22 +0530

    Basic UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // Disables Debug Banner
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.blue[300],
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.blue[200],
+              borderRadius: BorderRadius.circular(15)
+            ),
+            padding: EdgeInsets.all(25),
+            child: Icon(Icons.shop_2_outlined, color: Colors.white,)
+          )
+        ),
+      ),
     );
   }
 }
