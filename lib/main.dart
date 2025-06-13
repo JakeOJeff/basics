9812c85458805ab8686b09a19db4ba497cd51a8c
JakeOJeff <realjakeojeff@gmail.com>
2025-06-13 21:05:38 +0530

    Edited

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,8 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  Map user = {'name': "Jake", 'id': 12};
   @override
   Widget build(BuildContext context) {
-    print(user['name']);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false, // Disables Debug Banner
       home: Scaffold(),
