a4460233fa6bbd76bd593a19dd487146756f112f
JakeOJeff <realjakeojeff@gmail.com>
2025-06-16 00:02:08 +0530

    Alighnment Stack

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,31 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue[300],
         
-        body: GridView.builder(
-          itemCount: 64,
-          gridDelegate: 
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:4),
-          itemBuilder: (context, index) => Container(
-            color: Colors.blue[100],
-            margin: EdgeInsets.all(3),
-          ),
-          
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+
+
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.blue[700]
+            ),
+
+
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.blue[600]
+            ),
+
+
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue[500]
+            )
+          ],
         ),
       ),
     );
