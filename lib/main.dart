9359ae73d6360c8c2d65999bfd8be85f3d0dbea8
JakeOJeff <realjakeojeff@gmail.com>
2025-06-15 19:13:45 +0530

    Column Learning

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,31 +13,28 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false, // Disables Debug Banner
       home: Scaffold(
         backgroundColor: Colors.blue[300],
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text("Testing Grounds", 
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            )
-          ),
-          leading: Icon(Icons.menu, color: Colors.white),
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app, color: Colors.white))
-          ],
+        
+        body: Column(
+          children: [
 
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.blue[200],
-              borderRadius: BorderRadius.circular(15)
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue,
             ),
-            padding: EdgeInsets.all(25),
-            child: Icon(Icons.shop_2_outlined, color: Colors.white, size: 64)
-          )
+
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue[200],
+            ),
+
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue[100],
+            ),
+          ]
         ),
       ),
     );
